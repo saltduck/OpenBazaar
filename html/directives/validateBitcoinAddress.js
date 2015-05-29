35c26425f2659560171633b5f4b341112fb1118a
Brian Hoffman <brianchoffman@gmail.com>
2015-05-29 16:53:51 -0400

    validateBTCAddress directive

---
+++ html/directives/validateBitcoinAddress.js
@@ -0,0 +1,30 @@
+angular.module('app').directive("validateBitcoinAddress", function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, ele, attrs, ctrl) {
+
+            ctrl.$parsers.unshift(function(value) {
+                if(value){
+                    // test and set the validity after update.
+                    var valid = window.bitcoinAddress.validate(value);
+                    ctrl.$setValidity('validateBitcoinAddress', valid);
+                }
+
+                // if it's valid, return the value to the model,
+                // otherwise return undefined.
+                return valid ? value : undefined;
+            });
+
+            ctrl.$formatters.unshift(function(value) {
+                // validate.
+                ctrl.$setValidity('validateBitcoinAddress', window.bitcoinAddress.validate(value));
+
+                // return the value or nothing will be written to the DOM.
+                return value;
+            });
+
+
+        }
+    }
+
+});
